ed9d8c8ad0540d0c7f3a8b41299b5d8c98182bdb
truongminhluong <truongminhluong2003@gmail.com>
2024-01-17 20:38:25 +0700

    Initial Commit

---
+++ DoAnTotNghiep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoAnTotNghiep
+//
+//  Created by Trương Minh Lượng Jr on 17/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
